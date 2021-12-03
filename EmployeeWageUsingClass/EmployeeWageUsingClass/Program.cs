488d00bc43881690b8fdc12677a047e32376983e
Rajangane <pratiksharajangane7@gmail.com>
2021-12-03 15:32:21 +0530

    [Pratiksha] ADD : Employee wage for multiple company

--- EmployeeWageUsingClass/EmployeeWageUsingClass/Program.cs
+++ EmployeeWageUsingClass/EmployeeWageUsingClass/Program.cs
@@ -3,4 +3,19 @@
 Console.WriteLine("************* Employee Wage Using Class ***************");
 */
 using EmployeeWageUsingClass;
- new ComputeEmployeeWage().ComputeWage();
+Console.Write("Select Number:\n1)ComputeEmployeeWage\n2)EmpMulttipleCompany\n");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        new ComputeEmployeeWage().ComputeWage();
+        break;
+    case 2:
+
+        new EmpMultipleCompany().Compute("Dmart", 10, 20, 30);
+        new EmpMultipleCompany().Compute("vishal", 10, 40, 50);
+        break;
+    default:
+        Console.WriteLine("\n invalid choice");
+            break;
+}
